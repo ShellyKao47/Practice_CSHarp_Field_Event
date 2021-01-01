416dfef55ce15a746f998e64cd175ea4e27c70bc
ShellyKao47 <75553497+ShellyKao47@users.noreply.github.com>
2021-01-01 17:31:09 +0800

    Script

---
+++ UnityProject/Assets/Script/Chicken.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    [Header("速度"), Range(1.5f, 500f)]
+    public float speed = 10.5f;
+    [Header("跳躍高度"), Range(10, 100)]
+    public int Jump = 50;
+    [Header("對話內容")]
+    public string Taik = "咕咕咕~~~";
+    [Header("是否取得雞蛋")]
+    public bool Egg = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("遊戲開始!!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("咕咕咕~");
+
+    }
+}
